7a40cc661218fa8bc44710343d9984f377212aa0
Darun Naeem Sejan <150942877+SejanStrong@users.noreply.github.com>
2025-09-10 16:27:33 +0600

    almost

--- script.js
+++ script.js
@@ -1,11 +1,104 @@
-const loadLession = () => {
-  fetch("https://openapi.programming-hero.com/api/plants")
-    .then((res) => res.json());
-    .then((data) => console.log(data));
+const loadcatagory = () => {
+    fetch("https://openapi.programming-hero.com/api/categories")
+        .then((cat) => cat.json())
+        .then((data) => displayCatagory(data.categories));
+
+};
+const cardload = (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/category/${id}`;
+
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+
+
+}
+
+const loadCard = () => {
+
+    const url = `https://openapi.programming-hero.com/api/category/${id}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then(data => displayCard(data.plants));
+    ````
+
 };
 
-//const displayLesson = (lessons) => {
-//console.log(lessons);
-//};
 
-loadLession()
+//"id": 1,
+//   "image": "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg",
+//     "name": "Mango Tree",
+//    "description": "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals.",
+//    "category": "Fruit Tree",
+//    "price": //
+const displayCard = (cards) => {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    for (const card of cards) {
+        console.log(card);
+        const cardDiv = document.createElement("div");
+        cardDiv.innerHTML = `
+        
+           <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img src ="${card.image}"/>
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${card.name}</h2>
+    <p>
+    
+    ${card.description}
+</p>
+    <h3>${card.category}</h3>
+    <p>${card.price}</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-primary">Add to card</button>
+    </div>
+  </div>
+</div>
+        
+        
+        `;
+        cardContainer.appendChild(cardDiv);
+    }
+}
+
+//${catagory.catagory_name}
+loadcatagory()
+
+const displayCatagory = (catagorys) => {
+
+    console.log(catagorys)
+    const catagoryContainer = document.getElementById("catagory-container")
+    for (const catagory of catagorys) {
+        console.log(catagory);
+        const catagoryBtn = document.createElement("button");
+
+        catagoryBtn.innerHTML = ` <button onlick="cardLoad()" class= "btn btn-outline btn-primary ">${catagory.category_name}</button>
+            `;
+        catagoryContainer.appendChild(catagoryBtn);
+
+    }
+}
+
+/* const loadlessons = () => {
+    fetch("https://openapi.programming-hero.com/api/plants")
+
+        .then((res) => res.json())
+        .then((data) => displaylessons(data.plants));
+
+};
+
+const displaylessons = (lessons) => {
+
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    for (const lesson of lessons) {
+        console.log(lesson);
+        const cardDiv = document.createElement("div");
+        cardDiv.innerHTML = ``
+    }
+}
+
+loadlessons()  */
